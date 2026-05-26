d7fb95639348ae7f1f9aecda5e1a8e7a42a28c0e
guilherme11azev <guilherme.azevti@gmail.com>
2026-05-26 11:24:01 -0300

    Testes unitários para CategoryService e TransactionService

--- obj/Debug/net10.0/FinanceControl.AssemblyInfo.cs
+++ obj/Debug/net10.0/FinanceControl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e030996cf9da4399459c8bb6609f0ca18a2b6e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
